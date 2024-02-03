2854ee79c01c213cf2e802a1673c88eeb6ba7338
Bhooyas <bak05102000@gmail.com>
2024-02-03 12:48:39 +0530

    Adding rotors and reflectors

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This is a github repository is a try to implement Enigma machine in python.
 
 ## Features
 - Plugbord implemented
+- Rotor implemented (Rotation Logic remaining)
+- Reflector implemented
 
 ## Testing
 
@@ -12,14 +14,9 @@ We will be testing the enigma machine on a Quote by Alan Turing.
 > “Those who can imagine anything, can create the impossible.”
 
 
-Plain Text: - `THOSEWHOCANIMAGINEANYTHINGCANCREATETHEIMPOSSIBLE`
-
-Rotors: - `I II III`
-
-Rotors Start: - `BAK`
-
-Refector: - `B`
-
-Plug Board: - `WV NO DM RS GH PL YE TJ IB KQ AZ`
-
+Plain Text: - `THOSEWHOCANIMAGINEANYTHINGCANCREATETHEIMPOSSIBLE`<br>
+Rotors: - `I II III`<br>
+Rotors Start: - `BAK`<br>
+Refector: - `B`<br>
+Plug Board: - `WV NO DM RS GH PL YE TJ IB KQ AZ`<br>
 Cipher Text: - `NGKWAXUFKDULKCQVQDYUTMNBHPASWUYXTFUMCWAUGXMZDNBT`
--- enigma.py
+++ enigma.py
@@ -9,8 +9,54 @@ class Plugboard:
 	def encrypt(self, letter):
 		return self.connection.get(letter, letter)
 
-plugboard = Plugboard(connection=["WV", "NO", "DM", "RS", "GH", "PL", "YE", "TJ", "IB", "KQ", "AZ"])
+class Rotor:
 
+	def __init__(self, connection):
+		self.input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		self.output = connection
+
+	def encrypt(self, letter, backward=False):
+		if backward:
+			return self.input[self.output.index(letter)]
+		return self.output[self.input.index(letter)]
+
+class Reflector:
+
+	def __init__(self, connection):
+		self.input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		self.output = connection
+
+	def encrypt(self, letter):
+		return self.output[self.input.index(letter)]
+
+plugboard = Plugboard(connection=["WV", "NO", "DM", "RS", "GH", "PL", "YE", "TJ", "IB", "KQ", "AZ"])
+ROTOR_I = Rotor(connection="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+ROTOR_II = Rotor(connection="AJDKSIRUXBLHWTMCQGZNPYFVOE")
+ROTOR_III = Rotor(connection="BDFHJLCPRTXVZNYEIWGAKMUSQO")
+ROTOR_IV = Rotor(connection="ESOVPZJAYQUIRHXLNFTGKDCMWB")
+ROTOR_V = Rotor(connection="VZBRGITYUPSDNHLXAWMJQOFECK")
+REFLECTOR_A = Reflector(connection="EJMZALYXVBWFCRQUONTSPIKHGD")
+REFLECTOR_B = Reflector(connection="YRUHQSLDPXNGOKMIEBFZCWVJAT")
+REFLECTOR_c = Reflector(connection="FVPJIAOYEDRZXWGCTKUQSBNMHL")
+
+l = "A"
+print(l)
+l = ROTOR_I.encrypt(l)
+print(l)
+l = ROTOR_II.encrypt(l)
+print(l)
+l = ROTOR_III.encrypt(l)
+print(l)
+l = REFLECTOR_B.encrypt(l)
+print(l)
+l = ROTOR_III.encrypt(l, backward=True)
+print(l)
+l = ROTOR_II.encrypt(l, backward=True)
+print(l)
+l = ROTOR_I.encrypt(l, backward=True)
+print(l)
+
+"""
 message = "THOSEWHOCANIMAGINEANYTHINGCANCREATETHEIMPOSSIBLE"
 cipher = ""
 
@@ -19,3 +65,4 @@ for i in message:
 
 print(message)
 print(cipher)
+"""
